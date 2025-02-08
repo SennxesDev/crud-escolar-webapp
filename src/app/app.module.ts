1712201609f820ccea5b7089bffbc74882df03e3
Senn <100888352+SennxesDev@users.noreply.github.com>
2025-02-07 18:52:12 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistroUsuariosScreenComponent } from './screens/registro-usuarios-screen/registro-usuarios-screen.component';
+import { FormsModule } from '@angular/forms';
+
+//Angular material 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginScreenComponent,
+    RegistroUsuariosScreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

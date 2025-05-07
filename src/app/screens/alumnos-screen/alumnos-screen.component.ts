a4ce12dae62391bffff67166e0d7483383f95d67
Senn <100888352+SennxesDev@users.noreply.github.com>
2025-05-07 13:36:30 -0600

    Modals y Eliminar
    Se creo un modal para hacer mas sencilla la eliminación de todos los tipos de usuarios, ya es un código funcional y fue probado tanto en el proyecto así como en postman

--- src/app/screens/alumnos-screen/alumnos-screen.component.ts
+++ src/app/screens/alumnos-screen/alumnos-screen.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { FacadeService } from 'src/app/services/facade.service';
 import { AlumnosService } from 'src/app/services/alumnos.service';
+import { EliminarUserModalComponent } from 'src/app/modals/eliminar-user-modal/eliminar-user-modal.component';
 
 
 @Component({
@@ -106,18 +107,23 @@ export class AlumnosScreenComponent implements OnInit{
   }
 
   // Eliminar alumno
-  public delete(idAlumno: number) {
-    if (confirm("¿Estás seguro de eliminar este alumno?")) {
-      this.alumnosService.eliminarAlumno(idAlumno).subscribe(
-        () => {
-          console.log("Alumno eliminado:", idAlumno);
-          this.obtenerAlumnos(); // Recargar la lista después de eliminar
-        },
-        (error) => {
-          alert("No se pudo eliminar el alumno");
-        }
-      );
-    }
+public delete(idUser: number){
+    const dialogRef = this.dialog.open(EliminarUserModalComponent,{
+      data: {id: idUser, rol: 'alumno'}, //Se pasan valores a través del componente
+      height: '288px',
+      width: '328px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result.isDelete){
+        console.log("Alumno eliminado");
+        //Recargar página
+        window.location.reload();
+      }else{
+        alert("Alumno no eliminado ");
+        console.log("No se eliminó el alumno");
+      }
+    });
   }
 
 }// Fin de la clase
